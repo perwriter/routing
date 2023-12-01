f6cd307110d3ff2ac976f7cda42c16e6cc8e5cc6
perwriters <perwriters@gmail.com>
2023-12-01 09:23:41 +0300

    Completed react routing

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Footer = () => {
+  return <div className="mt-3 border-top border-dark">Footer</div>;
+};
+
+export default Footer;
